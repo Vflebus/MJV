262a7d45adf8c48102dd7731f12eccfdde0aac0b
Vincent Flebus <vflebus@gmail.com>
2025-07-09 13:05:38 +0200

    removed white bg on discord link

--- components/ThirdPanel.vue
+++ components/ThirdPanel.vue
@@ -1,36 +1,37 @@
 <script setup>
-import gsap from 'gsap';
+import gsap from "gsap";
 
 let ctx;
 
 onMounted(() => {
-    ctx = gsap.context((self) => {
-        gsap.from('#infosComplementaires', {
-            rotation: -90,
-            transformOrigin: 'bottom center',
-            scrollTrigger: {
-                trigger: '#panel3',
-                start: 'top bottom',
-                end: 'bottom bottom',
-                scrub: true,
-                invalidateOnRefresh: true,
-                // markers: true
-            },
-        })
-    }, '#main'); // <- Scope!
+  ctx = gsap.context((self) => {
+    gsap.from("#infosComplementaires", {
+      rotation: -90,
+      transformOrigin: "bottom center",
+      scrollTrigger: {
+        trigger: "#panel3",
+        start: "top bottom",
+        end: "bottom bottom",
+        scrub: true,
+        invalidateOnRefresh: true,
+        // markers: true
+      },
+    });
+  }, "#main"); // <- Scope!
 });
 
 onUnmounted(() => {
-    ctx.revert(); // <- Easy Cleanup!
+  ctx.revert(); // <- Easy Cleanup!
 });
 </script>
 
 <template>
-    <div class="w-full h-screen relative flex flex-col justify-center items-center overflow-hidden" id="panel3">
-        <div class="relative overflow-visible w-[90%] landscape:h-[85%] landscape:w-[unset]" id="infosComplementaires">
-            <img src="/images/infosPratiques.png" alt="graph de la MJV" class="w-full landscape:w-[unset] landscape:h-full">
-            <a href="https://discord.gg/v35ZARDXWh"><img src="/images/discordLogo.png" alt=""
-                    class="absolute bottom-[5%] right-[10%] w-[50%] bg-white/50 rounded-sm shadow-[0_4px_6px_rgba(255,255,255,0.5)]"></a>
-        </div>
+  <div class="w-full h-screen relative flex flex-col justify-center items-center overflow-hidden" id="panel3">
+    <div class="relative overflow-visible w-[90%] landscape:h-[85%] landscape:w-[unset]" id="infosComplementaires">
+      <img src="/images/infosPratiques.png" alt="graph de la MJV" class="w-full landscape:w-[unset] landscape:h-full" />
+      <a href="https://discord.gg/v35ZARDXWh">
+        <img src="/images/discordLogo.png" alt="" class="absolute bottom-[5%] right-[10%] w-[50%] rounded-sm shadow-[0_4px_6px_rgba(255,255,255,0.5)]" />
+      </a>
     </div>
+  </div>
 </template>
